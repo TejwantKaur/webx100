328d7bddc9c52ebcca3f858d3a7bdaaa1c0624a6
Tejwant Kaur <kaurtejwant998@gmail.com>
2026-09-08 12:28:35 +0530

    connected with mongoose db

---
+++ 05weekReact/todo-fullstack/backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+mongoose.connect(
+    'mongodb+srv://kaurtejwant998_db_user:U3UPOV2YnaKHAU6A@cluster0.1vfzbyb.mongodb.net/Todos'
+);
+
+const todoSchema = mongoose.Schema({
+    title: String, 
+    description: String,
+    completed: Boolean
+})
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.export = { Todo }
