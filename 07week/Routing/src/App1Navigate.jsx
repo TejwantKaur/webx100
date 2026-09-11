b63da1f312f24ff638b5df7686a7789f6533b5b0
Tejwant Kaur <kaurtejwant998@gmail.com>
2026-09-11 12:23:25 +0530

    useNavigate to switch btw pages; components

---
+++ 07week/Routing/src/App1Navigate.jsx
@@ -0,0 +1,30 @@
+import { lazy } from 'react'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import './App.css'
+const Dashboard = lazy(() => import ('./components/Dashboard'))
+const Landing = lazy(() => import ('./components/Landing'))
+
+// client side routing
+function App1Navigate() {
+  return (
+    <BrowserRouter>
+    <AppBar />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path='/' element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function AppBar(){
+  const navigate = useNavigate() // hook
+  return(
+    <div>
+        <button onClick={()=> {navigate('/')}}>Landing Page</button>
+        <button onClick={()=> {navigate('/dashboard')}}>Dashboard</button>
+    </div>
+  )
+}
+
+export default App1Navigate;
