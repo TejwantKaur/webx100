73cc43dfb10a1a8f05e55d6d0703d14d350ed83c
Tejwant Kaur <kaurtejwant998@gmail.com>
2026-09-17 07:47:35 +0530

    connection without function

--- 08week/paytm/backend/db.js
+++ 08week/paytm/backend/db.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const mongo_url = "mongodb+srv://kaurtejwant998_db_user:D1gYKtJptyafhpAN@paytmcluster.ina5mgz.mongodb.net/Paytm"
 
-async function connectDB(){
-  await mongoose.connect(mongo_url);
-  console.log("DB Connected!")
-}
+// async function connectDB(){
+  try {
+    await mongoose.connect(mongo_url);
+    console.log("DB Connected!")
+  }
+  catch(err){
+    console.log("Couldn't connect to DB ")
+    console.error(err.message)
+  }
+// }
 
 //   async function connectDB() {
 //     await mongoose.connect(MONGO_URL);
@@ -29,25 +35,26 @@ const userSchema = new Schema({
     minLength: 3,
     maxLength: 30,
   },
+
   password: {
     type: String,
     required: true,
-    minLength: 6,
+    minLength: 4,
     maxLength: 30,
   },
+
   firstName: {
     type: String,
     required: true,
     trim: true,
-    lowercase: true,
     minLength: 3,
     maxLength: 30,
   },
+
   lastName: {
     type: String,
     required: true,
     trim: true,
-    lowercase: true,
     minLength: 3,
     maxLength: 30,
   },
@@ -56,6 +63,7 @@ const userSchema = new Schema({
 // another db that will store the balance of user in mongo db;
 const accountSchema = new mongoose.Schema({
   userId: {
+    // useId jo mongo e bnonda ohi vali;
     type: mongoose.Schema.Types.ObjectId, // id stored in mongoose as object id's
     ref: 'User', // ensures userId must belong to User table; only then we can add balance;
     required: true
