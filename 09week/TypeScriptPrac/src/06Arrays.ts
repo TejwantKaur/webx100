966528b77eb9603ab53cad4c34e327b671aa6221
Tejwant Kaur <kaurtejwant998@gmail.com>
2026-09-18 12:09:08 +0530

    Arrays

---
+++ 09week/TypeScriptPrac/src/06Arrays.ts
@@ -0,0 +1,18 @@
+function maxValue(arr: number[]){
+
+}
+
+type numberArr = number[];
+function maxVal2(arr: numberArr){
+
+}
+
+maxValue([1,2,3]);
+
+interface User{
+
+}
+
+function filterUsers(users: User[]){ // users is the array of type User
+
+}
